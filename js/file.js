54d35ddcab270f0316fe5ca4c9f8d48a22b3ee16
xiaochong <915720461@qq.com>
2018-05-05 12:46:38 +0800

    5/5

--- js/file.js
+++ js/file.js
@@ -1,3 +1,35 @@
 function saveTofile(){
-    alert(111);
+
+    document.getElementById("popup").style.width = "300px";
+    document.getElementById("popup").style.top = "0px";
+    document.getElementById("popup").style.left = "0px";
+
+    document.getElementById("popuptext").innerHTML =
+        "<h4>导入</h4>" +
+        "<div class=\"form-wrapper\">" +
+        "<form method='post'>" +
+        "<input style='max-width: 90%'  type=\"file\" name=\"text\" accept='text/plain'/>" +
+        "</form></div>" +
+        "<div>" +
+        "<button type=\"button\" class='btn btn-success' value=\"Yes\" id=\"upyes\">确定</button>" +
+        "<button type=\"button\" class='btn btn-warning' value=\"No\" id=\"upno\" >取消</button>" +
+        "</div>";
+
+
+    $("#upyes").on("click", function () {
+        $("#popupwrap").hide();
+        $("#popupbackground").fadeOut(400);
+    });
+
+    $("#upno").on("click", function () {
+        $("#popupwrap").hide();
+        $("#popupbackground").fadeOut(400);
+    });
+
+
+    // Show using animation.
+    $("#popupbackground").fadeIn(400, function() {
+        $("#popupwrap").show();
+    });
+    
 }
